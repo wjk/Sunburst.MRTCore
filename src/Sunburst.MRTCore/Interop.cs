df5f1178db2cacfebed6b5ea191df57546c73a2e
William Kent <wjk011@gmail.com>
2026-08-20 11:15:17 -0400

    Initial commit of Sunburst.MRTCore project
    Due to unavoidable issues concerning side-by-side registration of the
    C++ WinRT classes in Microsoft.ApplicationModel.Resources.dll, I must
    rewrite that code in C#. Fortunately, it is not very complicated, and
    it uses the flat C MRM.dll API, which is P/Invoke friendly.

---
+++ src/Sunburst.MRTCore/Interop.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Microsoft.ApplicationModel.Resources;
+
+internal static class NativeMethods
+{
+
+}
---
+++ src/Sunburst.MRTCore/KnownResourceQualifierNames.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.ApplicationModel.Resources
+{
+    public static class KnownResourceQualifierNames
+    {
+        public static readonly string Contrast = "Contrast";
+        public static readonly string Custom = "Custom";
+        public static readonly string DeviceFamily = "DeviceFamily";
+        public static readonly string HomeRegion = "HomeRegion";
+        public static readonly string Language = "Language";
+        public static readonly string LayoutDirection = "LayoutDirection";
+        public static readonly string Scale = "Scale";
+        public static readonly string TargetSize = "TargetSize";
+        public static readonly string Theme = "Theme";
+    }
+}

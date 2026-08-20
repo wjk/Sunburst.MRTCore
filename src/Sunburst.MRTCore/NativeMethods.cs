369cc9ccb9dafac9e66cd71ca0af2055f5b1d0e8
William Kent <wjk011@gmail.com>
2026-08-20 13:00:48 -0400

    Add error-code handling functions to NativeMethods
    This was in Helpers.cpp in the C++ original.

--- src/Sunburst.MRTCore/NativeMethods.cs
+++ src/Sunburst.MRTCore/NativeMethods.cs
@@ -4,6 +4,38 @@ namespace Microsoft.ApplicationModel.Resources;
 
 internal static class NativeMethods
 {
+    #region Error Codes
+
+    public static readonly int E_NOT_FOUND = HRESULT_FROM_WIN32(1168);
+    public static readonly int E_FILE_NOT_FOUND = HRESULT_FROM_WIN32(2);
+    public static readonly int E_PATH_NOT_FOUND = HRESULT_FROM_WIN32(3);
+    public static readonly int E_MRM_MAP_NOT_FOUND = HRESULT_FROM_WIN32(15135);
+    public static readonly int E_MRM_NAMED_RESOURCE_NOT_FOUND = HRESULT_FROM_WIN32(15127);
+    public static readonly int E_MRM_NO_CANDIDATE = HRESULT_FROM_WIN32(15115);
+    public static readonly int E_MRM_NO_MATCH_OR_DEFAULT_CANDIDATE = HRESULT_FROM_WIN32(15116);
+
+    public static bool IsResourceNotFound(int hr)
+    {
+        return hr == E_NOT_FOUND || hr == E_FILE_NOT_FOUND || hr == E_PATH_NOT_FOUND ||
+            hr == E_MRM_MAP_NOT_FOUND || hr == E_MRM_NAMED_RESOURCE_NOT_FOUND ||
+            hr == E_MRM_NO_CANDIDATE || hr == E_MRM_NO_MATCH_OR_DEFAULT_CANDIDATE;
+    }
+
+    private static int HRESULT_FROM_WIN32(int error)
+    {
+        if (error <= 0)
+        {
+            return error;
+        }
+        else
+        {
+            const int FACILITY_WIN32 = 7;
+            return unchecked((int)((error & 0xFFFF) | (FACILITY_WIN32 << 16) | 0x80000000));
+        }
+    }
+
+    #endregion
+
     public struct MrmResourceData
     {
         public uint size;
